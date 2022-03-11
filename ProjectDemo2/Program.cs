8c01c57e40b767955d3dbbad16e6174d26a7b993
69bucks <95746707+69bucks@users.noreply.github.com>
2022-03-11 13:25:16 +0700

    Add files via upload

---
+++ ProjectDemo2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho va ten: Nguyenn Nhon Hoa");
+            Console.WriteLine("MSSV: 19dh111074");
+        }
+    }
+}
